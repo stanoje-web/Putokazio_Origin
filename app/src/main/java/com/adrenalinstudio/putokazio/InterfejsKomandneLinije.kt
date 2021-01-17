0deee7add8119c0f0c4e81d6ed1ebcecd3d3c911
Paunnn <61233251+Paunnn@users.noreply.github.com>
2021-01-17 17:56:52 +0100

    Linux

---
+++ app/src/main/java/com/adrenalinstudio/putokazio/InterfejsKomandneLinije.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class InterfejsKomandneLinije : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_interfejs_komandne_linije)
+    }
+}
---
+++ app/src/main/java/com/adrenalinstudio/putokazio/LinuxuDanas.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LinuxuDanas : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_linuxu_danas)
+    }
+}
