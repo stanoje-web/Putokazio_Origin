81927a97c19144d18115340593c81554b91373c5
Nikola Prodic <61201399+Proda053@users.noreply.github.com>
2021-01-17 17:54:13 +0100

    HTML
    Dodao sadržaj za HTML

---
+++ app/src/main/java/com/adrenalinstudio/putokazio/HTML.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HTML : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_h_t_m_l)
+    }
+}
