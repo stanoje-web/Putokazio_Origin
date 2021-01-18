bbc1b2128fc983ec1a83b414a0be5ec841e9227a
Paunnn <61233251+Paunnn@users.noreply.github.com>
2021-01-18 14:17:55 +0100

    Kernel

--- app/src/main/java/com/adrenalinstudio/putokazio/GrafickiProgrami.kt
+++ app/src/main/java/com/adrenalinstudio/putokazio/GrafickiProgrami.kt
@@ -11,11 +11,19 @@ class GrafickiProgrami : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_graficki_programi)
+        val sharedPreferences=getSharedPreferences("sharedPref", MODE_PRIVATE)
+        val savedBoolean=sharedPreferences.getBoolean("pdf", false)
+        val savedBoolean2=sharedPreferences.getBoolean("prikazivacislika", false)
+        val savedBoolean3=sharedPreferences.getBoolean("gimp", false)
+        val savedBoolean4=sharedPreferences.getBoolean("colorchooser", false)
         val pdf=findViewById<Button>(R.id.button20)
         val prikazivacislika=findViewById<Button>(R.id.button21)
         val gimp=findViewById<Button>(R.id.button22)
         val colorchooser=findViewById<Button>(R.id.button9)
-
+        val pdfbrt=intent.getBooleanExtra("pdfbt",false)
+        val prikazivacislikabrt=intent.getBooleanExtra("prikazivacislikabt",false)
+        val gimpbrt=intent.getBooleanExtra("gimpbt",false)
+        val colorchooserbrt=intent.getBooleanExtra("colorchooserbt",false)
         val back=findViewById<ImageButton>(R.id.imageButton1)
         back.setOnClickListener {
             val intent= Intent(this,LinuxServeri::class.java)
@@ -36,6 +44,30 @@ class GrafickiProgrami : AppCompatActivity() {
         colorchooser.setOnClickListener {
             val intent= Intent(this,ColorChooser::class.java)
             startActivity(intent)
+        }
+        if(savedBoolean==true) {
+            pdf.setBackgroundColor(Color.rgb(89, 171, 43))
+        }
+        else{
+
+        }
+        if(savedBoolean2==true) {
+            prikazivacislika.setBackgroundColor(Color.rgb(89, 171, 43))
+        }
+        else{
+
+        }
+        if(savedBoolean3==true) {
+            gimp.setBackgroundColor(Color.rgb(89, 171, 43))
+        }
+        else{
+
+        }
+        if(savedBoolean4==true) {
+            colorchooser.setBackgroundColor(Color.rgb(89, 171, 43))
+        }
+        else{
+
         }
     }
 }
--- app/src/main/java/com/adrenalinstudio/putokazio/LinuxovKernel.kt
+++ app/src/main/java/com/adrenalinstudio/putokazio/LinuxovKernel.kt
@@ -3,16 +3,28 @@ package com.adrenalinstudio.putokazio
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
 class LinuxovKernel : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_linuxov_kernel2)
+        val najnovijiKernel=findViewById<Button>(R.id.button20)
+        val prilagodjavanjekernela=findViewById<Button>(R.id.button21)
         val back=findViewById<ImageButton>(R.id.imageButton1)
+        najnovijiKernel.setOnClickListener {
+            val intent= Intent(this,NajnovijiKernel::class.java)
+            startActivity(intent)
+        }
+        prilagodjavanjekernela.setOnClickListener {
+            val intent= Intent(this,PrilagodjavanjeKernela::class.java)
+            startActivity(intent)
+        }
         back.setOnClickListener {
             val intent= Intent(this,LinuxServeri::class.java)
             startActivity(intent)
+
         }
     }
 }
