109bb790e12011b7c6c5e19b967ff232d282d53f
Nikola Prodic <61201399+Proda053@users.noreply.github.com>
2021-01-19 16:29:37 +0100

    Polovina interneta

---
+++ app/src/main/java/com/adrenalinstudio/putokazio/HTTP.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HTTP : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_h_t_t_p)
+    }
+}
