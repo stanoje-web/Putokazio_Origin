ff512d7f72996163797d73591d60571589b873f4
Nikola Prodic <61201399+Proda053@users.noreply.github.com>
2021-01-20 06:00:13 +0100

    Ostatak interneta
    Ostala samo JavaScript za FrontEnd

---
+++ app/src/main/java/com/adrenalinstudio/putokazio/dns.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class dns : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_dns)
+    }
+}
