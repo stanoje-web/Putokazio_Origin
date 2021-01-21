42f213c393961d191da2f58c3d9903901cc10925
Nikola Prodic <61201399+Proda053@users.noreply.github.com>
2021-01-21 14:24:43 +0100

    Backend jezici
    Internet je već urađen

---
+++ app/src/main/java/com/adrenalinstudio/putokazio/CSharp.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CSharp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_c_sharp)
+    }
+}
---
+++ app/src/main/java/com/adrenalinstudio/putokazio/sql.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sql : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sql)
+    }
+}
