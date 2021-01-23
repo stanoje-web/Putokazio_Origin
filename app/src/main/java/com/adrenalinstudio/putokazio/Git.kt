3e4df4a20a1fd3e16d6b0fbc46aa90386460e298
stanoje-web <nikola@adrenalinstudio.com>
2021-01-23 15:25:06 +0100

    Sistemi za kontrolu verzija

---
+++ app/src/main/java/com/adrenalinstudio/putokazio/Git.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Git : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git)
+    }
+}
---
+++ app/src/main/java/com/adrenalinstudio/putokazio/KomandnaLinija.kt
@@ -0,0 +1,11 @@
+package com.adrenalinstudio.putokazio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class KomandnaLinija : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_komandna_linija)
+    }
+}
